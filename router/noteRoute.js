6af49c36df5638d7b7fb1e2e8ebc9a9126136502
Claryngweyee <161452606+Claryquinnzle22@users.noreply.github.com>
2025-09-14 16:52:59 +0700

    remove-temp-function

--- router/noteRoute.js
+++ router/noteRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
 import storeTemp from "../controller/note/storeTemp.js";
 import { checkAuth } from "../controller/auth/jwt.js";
+import removeTemp from "../controller/note/removeTemp.js";
 
 const noteRouter = express.Router();
 
 noteRouter.post("/convert", checkAuth, storeTemp);
+noteRouter.delete("/deleteTemp/:key", checkAuth, removeTemp);
 
 export default noteRouter;
