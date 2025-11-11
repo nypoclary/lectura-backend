2d8fc288f43c9fe8e67b2113a7d1dfff92b65e4d
False10101 <minph7102004@gmail.com>
2025-11-11 09:23:44 +0700

    VPS sync

--- index.js
+++ index.js
@@ -8,17 +8,22 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://lectura.minpainghein.com",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   })
 );
+app.options("*", cors());
+
 
 app.use(cookieParser());
 
 app.use("/api", mainRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(5000, "0.0.0.0" ,() => {
+  console.log(`Server is running on port 5000`);
 });
